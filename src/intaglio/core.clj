cb4d186368546c5210166535d147a77dcc26611b
Tyler <tyler@brava.do>
2013-09-19 19:23:10 -0700

    * src/intaglio/core.clj:

--- src/intaglio/core.clj
+++ src/intaglio/core.clj
@@ -3,10 +3,16 @@
              [cheshire.core :refer :all]
              [markdown.core :as md])
   (:import
-   (java.util Date UUID)))
+   (java.util Date UUID)
+   (java.io StringReader)))
 
 (use 'clojure.pprint)
 
+
+(def config-atom (atom (parse-string (slurp "config.json") true)))
+
+(defn log-config []
+  (spit "config-log.json" (generate-string @config-atom) :append true))
 
 (defn write-config []
   (spit "config.json" (generate-string @config-atom))
@@ -14,24 +20,19 @@
 
 (def test-post (parse-string (slurp "testparse.json" ) true))
 
-(def config-atom (atom (parse-string (slurp "config.json") true)))
-
 (defn inc-post []
   (swap! config-atom update-in [:total-posts]  inc))
 
-(defn log-config []
-  (spit "config-log.json" (generate-string @config-atom) :append true))
-
-(defn add-post [post]
+(defn write-post [post]
   (inc-post)
   (def file-name (str (@config-atom :directory) "/entry-" (@config-atom :total-posts) ".md"))
   (spit file-name (post :markdown))
   (write-config)
   file-name)
 
-(defn format-post [raw-post]
-  (dissoc (assoc raw-post
-            :file (add-post raw-post)
+(defn format-post [post]
+  (dissoc (assoc post
+            :file (write-post post)
             :date (Date. )
             :timestamp (System/currentTimeMillis)
             :uuid (str (java.util.UUID/randomUUID))
@@ -39,25 +40,51 @@
 
 
 (defn append-posts-file [formatted-post]
- (spit (@config-atom :posts-file)
-       (generate-string {:entry-num (@config-atom :total-posts)
-                         :entry formatted-post} {:date-format "MM-dd-yyyy-H-m"})))
+  (spit (@config-atom :posts-file)
+    (generate-string
+      (conj (parse-string (slurp (@config-atom :posts-file)))
+            {:entry-num (@config-atom :total-posts)
+             :entry formatted-post}) {:date-format "MM-dd-yyyy-H-m"})))
 
+
+(defn post-html [{file :file}]
+  ((fn [html-string] (html/html-resource (StringReader. html-string)))
+  (md/md-to-html-string (slurp file))))
+
+(post-html {:file "posts/entry-9.md"})
+
+(pprint (conj (post-html {:file "posts/entry-9.md"}) (post-html {:file "posts/entry-9.md"})))
+
+(def p-col (partition-all (@config-atom :per-page)
+                       (reverse (sort-by :entry-num (parse-string (slurp "posts.json") true)))))
 
 (append-posts-file (format-post test-post))
   
 (html/deftemplate main-template "templates/skeleton.html"
   []
-   [:id#master] (html/content (:markdown make-config-map)))
+   [:id#master] (html/content ()))
+
+(pprint (conj (post-snippet (md/md-to-html-string (slurp "posts/entry-41.md"))) (post-snippet (md/md-to-html-string (slurp "posts/entry-41.md")))))
 
 (html/defsnippet post-snippet "templates/post.html"
   [:div.row]
-  [post-md]
-  [:div] (html/content post-md ))
+  [post]
+  [:div] (html/content (html-enlive post)))
 
+(gather-posts [(gather-posts [(gather-posts [(gather-posts [(gather-posts [(gather-posts [(gather-posts [(gather-posts [(gather-posts [
+  
+(defn make-page [page-num posts-map]
+  (main-template page-num
 
+(defn html-enlive 
+  [html-string]
+    (html/html-resource (StringReader. html-string)))
 
-(pprint (post-snippet (slurp "posts/somepost.html")))
+(defn write-html [{:keys [output-dir file-name posts]}]
+  (spit (str output-dir "/" file-name)
+        (apply str (html/emit* (main-template posts)))))
+
+(spit "test.html"  (apply str (html/emit* (post-snippet (slurp "posts/somepost.html")))))
 
 (def postys (html/html-resource "templates/post.html"))
 (pprint postys)
